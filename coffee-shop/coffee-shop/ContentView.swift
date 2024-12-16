2bb7e01f887578931ee8b3707a8cebbeb089bcc1
t-anastasiia <nastaytalmazan@gmail.com>
2024-12-16 20:30:27 +1000

    Initial commit

---
+++ coffee-shop/coffee-shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coffee-shop
+//
+//  Created by Анастасия Талмазан on 2024-12-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coffee-shop/coffee-shop/coffee_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_shopApp.swift
+//  coffee-shop
+//
+//  Created by Анастасия Талмазан on 2024-12-16.
+//
+
+import SwiftUI
+
+@main
+struct coffee_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
